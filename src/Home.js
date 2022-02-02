6124997107a8e823145fe38fdb58469a143ff755
Fabian B <lince.86@gmail.com>
2022-02-02 18:28:37 -0500

    06 | Using State (useStateHook)

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,27 @@
-// Click Events: Creating an event when the user clicks a button
+// Using State (useState Hook): Make a value reactive so when it changes, react detects that and it re-renders the template to see this new value in the browser
+
+// 01. Import the useState Hook
+import { useState } from "react";
 
 const Home = () => {
 
-  // 01. Create a function that will fire when the user click
+  // 02. Set the useState, it means, the initial state (name and age)
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  // 03. In the function, set the new name and the new age
   const handleClick = () => {
-    console.log('Hello ninjas');
+    setName('luigi');
+    setAge(30);
   }
 
-  // 03. Set a function with a parameter, in this case, a name
-  const handleClickAgain = (name) => {
-    console.log('Hello ' + name)
-  }
 
   return ( 
     <div className="home">
       <h2>Homepage</h2>
-      {/* 02. Create the button and set the event onClick */}
+      {/* 04. These are the dynamic values that will change when the user click the button */}
+      <p>{ name } is { age } years old</p>
       <button onClick={ handleClick }>Click me</button>
-      {/* 05. In this new button set the event onClick. The parameter name is into an anonymus function  06. Invoke the function handleClickAgain*/}
-      <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
     </div>
   );
 }
