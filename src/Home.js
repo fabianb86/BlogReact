dfa32c57118ecb1c3bc089a1acf7d9e0d89cf5ea
Fabian B <lince.86@gmail.com>
2022-02-02 18:53:35 -0500

    07 | Outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,26 @@
-// Using State (useState Hook): Make a value reactive so when it changes, react detects that and it re-renders the template to see this new value in the browser
+// Outputting lists: How to show all the items cycling each one.
 
 // 01. Import the useState Hook
 import { useState } from "react";
 
 const Home = () => {
 
-  // 02. Set the useState, it means, the initial state (name and age)
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  // 03. In the function, set the new name and the new age
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
-
-
+  // 02. Set the useState, it means, the initial state (initial blogs)
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+  ]);
+  
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      {/* 04. These are the dynamic values that will change when the user click the button */}
-      <p>{ name } is { age } years old</p>
-      <button onClick={ handleClick }>Click me</button>
+      {/* 03. Using JavaScript, with the map method cycle all the blogs to show in this template */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
