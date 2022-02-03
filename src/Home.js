62170a4a9751545b7ac1c3aa75129929e8c32ccb
Fabian B <lince.86@gmail.com>
2022-02-03 14:03:27 -0500

    13 | Fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,27 @@
-// using Json server: In order to use a database to fetch data into this component
+// Fetching data with useEffect: Set a get request to fetch all blogs
 
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+  // 01. Set null to the initial state
+  const [blogs, setBlogs] = useState(null);
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ]);
-
-  
-  const handleDelete = (id) =>{
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-  
-  
   useEffect(() =>{
-    console.log('use effect ran');
-    
+    // 02. Fetching data
+    fetch('http://localhost:8000/blogs')
+      .then(res =>{
+        return res.json();
+      })
+      .then((data) =>{
+        setBlogs(data);
+      });
   }, []);
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+      {/* 03. Combat the null error with the "and" conditional */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 }
