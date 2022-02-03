7459639fe94f0a50c288fdfba6777e408ae19683
Fabian B <lince.86@gmail.com>
2022-02-03 14:50:54 -0500

    15 | Handiling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,42 @@
-// Conditional Loading Message: Set a message to the user while we fetch the data
+// Handiling fetch errors: Show an error to the user if anything goes wrong with the fetching of data
 
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  // 01. Set the state of pending
   const [isPending, setIsPending] = useState(true);
+  // 03. Set the initial state for the error
+  const [error, setError] = useState(null);
 
   useEffect(() =>{
     fetch('http://localhost:8000/blogs')
       .then(res =>{
+        // 02. if statement to check if the response is not ok (false)
+        if(!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
         return res.json();
       })
       .then((data) =>{
         setBlogs(data);
-        // 03. When we fetch the data, set the isPending "false"
         setIsPending(false);
-      });
+        // 05. If is not an error
+        setError(null);
+      })
+      // 01. Catch the error
+      .catch(err =>{
+        setIsPending(false);
+        setError(err.message);
+      })
   }, []);
 
   return ( 
     <div className="home">
-      {/* 02. Set a conditional logical template to say when only isPending is true, will display the message */}
-      { isPending && <div>Loading... </div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {/* 04. Show the error in the browser */}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading... </div> }
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
     </div>
   );
 }
