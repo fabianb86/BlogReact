6cf11a52473795beb10ef78b611d9ad1ea8f1ca5
Fabian B <lince.86@gmail.com>
2022-02-03 13:22:14 -0500

    11 | useEffect hook & dependencies

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,4 @@
-// Functions as props: Delete an item
 
-// 04. Pass the handleDelete function as a prop
 const BlogList = ({ blogs, title, handleDelete}) => {
 
     return ( 
@@ -10,7 +8,6 @@ const BlogList = ({ blogs, title, handleDelete}) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
-          {/* 01. Create a button to delete a blog with an onClick event */}
           <button onClick={() => handleDelete(blog.id)} >Delete Blog</button>
         </div>
       ))}
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,7 @@
-import { useState } from "react";
+// useEffect: This hook runs a function every render of the components
+
+// 01. Import useEffect
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,16 +12,28 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
   ]);
 
-  // 02. Define the handleDelete function here
+  // 04. Another state
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) =>{
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
   
+  // 02. Set useEffect to 
+  useEffect(() =>{
+    console.log('use effect ran');
+    console.log(name);
+    // 03. If you put an empty array as a dependency as a second parameter, the effect will run just once after the intial render
+    // 06. If you put the name as a dependency, the effect will apply just to the name state
+  }, [name]);
+
   return ( 
     <div className="home">
-    {/* 03. Pass the handleDelete as a prop */}
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+      {/* 05. Button to change name */}
+      <button onClick={() => setName('luigi')}>Change Name</button>
+      <p>{ name }</p>
     </div>
   );
 }
