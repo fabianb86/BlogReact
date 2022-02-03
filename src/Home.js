be198a8424ca8db90ee3a8fa48ce9c37f22ddc09
Fabian B <lince.86@gmail.com>
2022-02-03 14:12:37 -0500

    14 | Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,29 @@
-// Fetching data with useEffect: Set a get request to fetch all blogs
+// Conditional Loading Message: Set a message to the user while we fetch the data
 
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  // 01. Set null to the initial state
   const [blogs, setBlogs] = useState(null);
+  // 01. Set the state of pending
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() =>{
-    // 02. Fetching data
     fetch('http://localhost:8000/blogs')
       .then(res =>{
         return res.json();
       })
       .then((data) =>{
         setBlogs(data);
+        // 03. When we fetch the data, set the isPending "false"
+        setIsPending(false);
       });
   }, []);
 
   return ( 
     <div className="home">
-      {/* 03. Combat the null error with the "and" conditional */}
+      {/* 02. Set a conditional logical template to say when only isPending is true, will display the message */}
+      { isPending && <div>Loading... </div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
